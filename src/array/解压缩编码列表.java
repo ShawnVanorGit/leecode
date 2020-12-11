48c15a45ccf8f07624eafa8389edef5ad7a41bdc
shawnvan <13631230887@163.com>
2020-12-11 22:09:00 +0800

    1

--- src/array/解压缩编码列表.java
+++ src/array/解压缩编码列表.java
@@ -1,4 +1,21 @@
 package array;
 
+
 public class 解压缩编码列表 {
+    public int[] decompressRLElist(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            len += nums[i];
+            i += 2;
+        }
+        int[] result = new int[len];
+        for (int i = 0; i < nums.length; i+=2) {
+            int n = nums[i];
+            int num = nums[i+1];
+            for (int j = 0; j < n; j++) {
+                list.add(num);
+            }
+        }
+        return list.stream().mapToInt(Integer::valueOf).toArray();
+    }
 }
