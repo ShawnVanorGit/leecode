bd624a208aa0ea0a8cec555145ea5697cfab1dab
INTERNET\180876 <13631230887@163.com>
2021-01-23 17:12:33 +0800

    1

---
+++ src/动态规划/medium/不同路径II.java
@@ -0,0 +1,32 @@
+package 动态规划.medium;
+
+/**
+ * @Author Natasha
+ * @Description
+ * @Date 2021/1/23 8:54
+ **/
+public class 不同路径II {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePathsWithObstacles(new int[][] {{0,1},{0,0}}));
+    }
+}
---
+++ src/动态规划/medium/最小路径和.java
@@ -0,0 +1,27 @@
+package 动态规划.medium;
+
+/**
+ * @Author Natasha
+ * @Description
+ * @Date 2021/1/23 12:18
+ **/
+public class 最小路径和 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
